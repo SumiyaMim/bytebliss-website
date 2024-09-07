3bb1ab4fa15875c6632011a705fbbaff99407c10
SumiyaMim <sumiyaislam1093@gmail.com>
2024-09-07 17:19:17 +0600

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Blogs from "./pages/Blogs"
+import Home from "./pages/Home"
+
+function App() {
+
+  return (
+    <>
+      <Home/>
+      <Blogs/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar"
+
+const Home = () => {
+  return (
+    <div className="h-20">
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Home
